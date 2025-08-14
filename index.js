77155bc8a8bcaef25d2988fa85371bb04f6954b2
md rasel islam <rashel099009@gamil.com>
2025-08-14 15:26:19 +0600

    added an index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+//---------01----------
+
+//  function totalFine(fare){
+//      if(typeof fare !== "number" || fare <= 0){
+//          return "Invalid";
+//      }
+//     const fine = fare + fare * (20 / 100)+30; 
+//     return fine;
+//  }
+
+//------------02-----------
+
+
+// function onlyCharacter(str){
+//         if(typeof str !== "string") return "Invalid"; 
+//         return str.replace(/\s+/g, "").toUpperCase();
+        
+// }
+
+//------------03-----------
+
+function bestTeam(player1, player2){
+    if(typeof player1 !== "object" || typeof player2 !== "object"){
+        return "Invalid";
+
+    }
+}
+    const player1 = {
+        name: "player1",
+        foul: 5,
+        cardYello: 2,
+        cardRead: 1
+    };
+    // const player2{
+    //     name: "player2",
+    //     foul: 7,
+    //     cardYello: 3,
+    //     cardRead: 0
+    // };
+
+matchInfo(player1);
+console.log(`player:${player1.name}`)
+
